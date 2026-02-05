c68ed6c10e247deb6822d38e468410e09d41e218
Giovanni Cardinali <giovanni.cardinali@studenti.unicam.it>
2026-02-05 12:09:54 +0100

    implemented part of risponde

--- src/main/java/unicam/model/utenti/mvc/UserController.java
+++ src/main/java/unicam/model/utenti/mvc/UserController.java
@@ -1,4 +1,23 @@
 package unicam.model.utenti.mvc;
 
+import unicam.model.inviti.Invito;
+import unicam.model.utenti.user.User;
+import unicam.model.utenti.user.repository.UserRepository;
+
 public class UserController {
+
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    public boolean risponde(boolean risposta, Invito invito, User user) {
+        userService.risponde(risposta, invito, user);
+    }
+
+    public boolean nuovoCoordinatore(User membroTeam) {
+        return true;
+    }
 }
--- src/main/java/unicam/model/utenti/mvc/UserService.java
+++ src/main/java/unicam/model/utenti/mvc/UserService.java
@@ -1,4 +1,37 @@
 package unicam.model.utenti.mvc;
 
+import unicam.model.inviti.Invito;
+import unicam.model.team.Team;
+import unicam.model.team.repository.InMemoryTeamRepository;
+import unicam.model.team.mvc.TeamService;
+import unicam.model.utenti.user.Ruoli;
+import unicam.model.utenti.user.User;
+
 public class UserService {
+
+    private TeamService teamService;
+    private InMemoryTeamRepository inMemoryTeamRepository;
+
+    public  TeamService getTeamService() {
+        return teamService;
+    }
+
+    public boolean risponde(boolean risposta, Invito invito, User user) {
+        //User's role
+        Ruoli r = user.getRuolo();
+        //Team that sent the request
+        Team mittente = inMemoryTeamRepository.getTeamById(invito.getTeamId());
+        //User's role
+        Team UserTeam = user.getTeam();
+
+        return risposta;
+    }
+
+    public boolean nuovoCoordinatore(User membroTeam) {
+        return true;
+    }
+
+    public boolean aggiungiMembro(Team team, User user) {
+        return true;
+    }
 }
