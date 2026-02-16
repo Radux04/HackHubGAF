0c1d05d5a4622820ac26ee40b8ad8a2949cf530d
Francesco Torchia <francesco.torchia@studenti.unicam.it>
2026-02-16 15:19:01 +0100

    definition of InMemorySottomissioniRepo.java

--- src/main/java/unicam/repository/InMemorySottomissioniRepo.java
+++ src/main/java/unicam/repository/InMemorySottomissioniRepo.java
@@ -1,4 +1,21 @@
 package unicam.repository;
 
-public class InMemorySottomissioniRepo {
+import unicam.model.hackathon.entity.Sottomissione;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemorySottomissioniRepo implements SottomissioniRepository {
+    private Map<Integer, Sottomissione> sottomissioniByid;
+
+
+    public InMemorySottomissioniRepo() {
+        sottomissioniByid = new HashMap<>();
+    }
+
+    @Override
+    public Sottomissione findbyid(int id) {
+        return sottomissioniByid.get(id);
+    }
+
 }
