4e3d93e45d9dc45d5e209a1b44855c477f0ecb58
Giovanni Cardinali <giovanni.cardinali@studenti.unicam.it>
2026-02-18 16:12:42 +0100

    IscrizioneTeamController updated

--- src/main/java/unicam/controller/IscrizioneTeamController.java
+++ src/main/java/unicam/controller/IscrizioneTeamController.java
@@ -21,7 +21,7 @@ public class IscrizioneTeamController {
 
     @PostMapping
     public Iscrizione iscriviTeam(@RequestBody IscrizioneDTO  iscrizioneDTO) {
-
+        //se il team non partecipa già a un hackathon lo aggiungo al DTO
         iscrizioneDTO.setTeamId(iscrizioneTeamService.controlloTeam(iscrizioneDTO.getCoordinatoreId()));
 
         return iscrizioneTeamService.iscriviTeam(iscrizioneDTO);
