159aeba31e4b8b2a73cb296f298ad5bda7e04fa9
Francesco Torchia <francesco.torchia@studenti.unicam.it>
2026-02-18 12:37:34 +0100

    JPA Repository

--- src/main/java/unicam/repository/TeamRepository.java
+++ src/main/java/unicam/repository/TeamRepository.java
@@ -2,8 +2,9 @@ package unicam.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import unicam.model.team.Team;
-
+@Repository
 public interface TeamRepository extends JpaRepository<Team,Long> {
     boolean existsByNome(String nome);
     Team findTeamByCoordinatoreId(Long coordinatoreId);
