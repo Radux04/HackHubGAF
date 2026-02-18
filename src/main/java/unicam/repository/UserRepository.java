ff07bf1a6bd3209723b9bc1a8b5b0e38b9a98336
Francesco Torchia <francesco.torchia@studenti.unicam.it>
2026-02-18 12:37:38 +0100

    JPA Repository

--- src/main/java/unicam/repository/UserRepository.java
+++ src/main/java/unicam/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package unicam.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import unicam.model.utenti.user.User;
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
