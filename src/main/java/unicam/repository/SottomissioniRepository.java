e3431f575854fbc430d796b79c67a95f3b79cf2c
Francesco Torchia <francesco.torchia@studenti.unicam.it>
2026-02-18 12:37:23 +0100

    JPA Repository

--- src/main/java/unicam/repository/SottomissioniRepository.java
+++ src/main/java/unicam/repository/SottomissioniRepository.java
@@ -2,8 +2,9 @@ package unicam.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import unicam.model.hackathon.entity.Sottomissione;
-
+@Repository
 public interface SottomissioniRepository extends JpaRepository<Sottomissione, Long> {
 
 }
