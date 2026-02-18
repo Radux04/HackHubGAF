09c24d0db5c613d8288b4f2f3b60204b321595f6
Giovanni Cardinali <giovanni.cardinali@studenti.unicam.it>
2026-02-18 18:47:17 +0100

    removed useless import

--- src/main/java/unicam/controller/TeamController.java
+++ src/main/java/unicam/controller/TeamController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import unicam.dto.team.CreaTeamDTO;
 import unicam.dto.team.InvitoDTO;
-import unicam.dto.team.RemoveMemberDTO;
 import unicam.model.inviti.Invito;
 import unicam.model.team.Team;
 import unicam.service.TeamService;
