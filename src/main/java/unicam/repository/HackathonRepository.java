69a233fe2373bfed2defbee51ff98ae8d249a5b8
Giovanni Cardinali <giovanni.cardinali@studenti.unicam.it>
2026-02-18 15:55:13 +0100

    Updated HackathonRepository

--- src/main/java/unicam/repository/HackathonRepository.java
+++ src/main/java/unicam/repository/HackathonRepository.java
@@ -3,8 +3,8 @@ package unicam.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import unicam.model.hackathon.entity.Hackathon;
 
-import java.util.List;
-
 public interface HackathonRepository extends JpaRepository<Hackathon, Long> {
+
+
     
 }
