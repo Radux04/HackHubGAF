a1aaf0c55663e5ff86d09993ffc23af84f08b1bd
Francesco Torchia <francesco.torchia@studenti.unicam.it>
2026-02-18 12:37:19 +0100

    JPA Repository

--- src/main/java/unicam/repository/RichiestaSupportoRepository.java
+++ src/main/java/unicam/repository/RichiestaSupportoRepository.java
@@ -1,10 +1,11 @@
 package unicam.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import unicam.model.supporto.RichiestaSupporto;
 
 import java.util.List;
-
+@Repository
 public interface RichiestaSupportoRepository extends JpaRepository <RichiestaSupporto, Long> {
     List<RichiestaSupporto> findByHackathonId(Long hackathonId);
 }
