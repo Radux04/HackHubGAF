dbdbc1c320da9884020ca07af2b633f7b34b474a
Francesco Torchia <francesco.torchia@studenti.unicam.it>
2026-02-18 12:37:15 +0100

    JPA Repository

--- src/main/java/unicam/repository/IscrizioneRepository.java
+++ src/main/java/unicam/repository/IscrizioneRepository.java
@@ -1,10 +1,11 @@
 package unicam.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import unicam.model.hackathon.entity.Hackathon;
 import unicam.model.iscrizione.Iscrizione;
 import unicam.model.team.Team;
-
+@Repository
 public interface IscrizioneRepository extends JpaRepository<Iscrizione, Long> {
 
     Long getHackatonByTeam(Long idTeam);
