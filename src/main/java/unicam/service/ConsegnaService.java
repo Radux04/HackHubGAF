2a67a78650f653d79fe123029241f5b00419bc7a
Francesco Torchia <francesco.torchia@studenti.unicam.it>
2026-02-18 11:17:39 +0100

    issue solved

--- src/main/java/unicam/service/ConsegnaService.java
+++ src/main/java/unicam/service/ConsegnaService.java
@@ -12,7 +12,7 @@ public class ConsegnaService {
 
     }
 
-    public boolean caricaSottomissione(int idSottomissione, String descrizione, int idIscrizione){
+    public boolean caricaSottomissione(Long idSottomissione, String descrizione, Long idIscrizione){
         Consegna c = new Consegna(idIscrizione, descrizione, idSottomissione);
         if(consegnaRepository.contains(c)){
             return false;
