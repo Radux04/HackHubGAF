b81a5525b66a3bcfa12ed35b183ccec53a55e418
Francesco Torchia <francesco.torchia@studenti.unicam.it>
2026-02-18 12:37:27 +0100

    JPA Repository

--- src/main/java/unicam/repository/StaffRepository.java
+++ src/main/java/unicam/repository/StaffRepository.java
@@ -1,10 +1,11 @@
 package unicam.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import unicam.model.utenti.staff.Staff;
 
 import java.util.Optional;
-
+@Repository
 public interface StaffRepository extends JpaRepository<Staff,Long> {
 
 }
