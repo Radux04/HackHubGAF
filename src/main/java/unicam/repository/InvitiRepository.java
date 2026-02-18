ccbc706e13d783215ca24b0eade1b4ad84d9b110
Francesco Torchia <francesco.torchia@studenti.unicam.it>
2026-02-18 12:37:02 +0100

    issue solved

--- src/main/java/unicam/repository/InvitiRepository.java
+++ src/main/java/unicam/repository/InvitiRepository.java
@@ -1,8 +1,9 @@
 package unicam.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import unicam.model.inviti.Invito;
-
+@Repository
 public interface InvitiRepository extends JpaRepository<Invito, Long> {
 
 }
