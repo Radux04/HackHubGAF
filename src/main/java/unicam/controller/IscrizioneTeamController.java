96398ce18ef79c5db067770a56267d390137637f
Radux04 <raducu.alessandro@gmail.com>
2026-02-19 15:10:01 +0100

    mediator cancelled

--- src/main/java/unicam/controller/IscrizioneTeamController.java
+++ src/main/java/unicam/controller/IscrizioneTeamController.java
@@ -9,34 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 import unicam.dto.iscrizione.IscrizioneDTO;
 import unicam.model.iscrizione.Iscrizione;
 import unicam.service.IscrizioneTeamService;
-import unicam.service.mediator.IscrizioneTeamMediator;
 
 
 @RestController
 @RequestMapping("/iscrizioneTeam")
 @RequiredArgsConstructor
 public class IscrizioneTeamController {
-    // Invece di iniettare direttamente il service, iniettiamo il mediator.
-//    private final IscrizioneTeamService iscrizioneTeamService;
-//
-//    @PostMapping
-//    public Iscrizione iscriviTeam(@RequestBody IscrizioneDTO  iscrizioneDTO) {
-//        //se il team non partecipa già a un hackathon lo aggiungo al DTO
-//        iscrizioneDTO.setTeamId(iscrizioneTeamService.controlloTeam(iscrizioneDTO.getCoordinatoreId()));
-//
-//        return iscrizioneTeamService.iscriviTeam(iscrizioneDTO);
-//    }
-
-    private final IscrizioneTeamMediator iscrizioneTeamMediator;
+    private final IscrizioneTeamService iscrizioneTeamService;
 
     @PostMapping
-    public Iscrizione iscriviTeam(@RequestBody IscrizioneDTO iscrizioneDTO) {
+    public Iscrizione iscriviTeam(@RequestBody IscrizioneDTO  iscrizioneDTO) {
+        //se il team non partecipa già a un hackathon lo aggiungo al DTO
+        iscrizioneDTO.setTeamId(iscrizioneTeamService.controlloTeam(iscrizioneDTO.getCoordinatoreId()));
 
-        // 1) uso il mediator per verificare se il team è libero
-        Long teamId = iscrizioneTeamMediator.verificaDisponibilitaTeam(iscrizioneDTO.getCoordinatoreId());
-        iscrizioneDTO.setTeamId(teamId);
-
-        // 2) poi chiamo il mediator per fare tutta l'iscrizione
-        return iscrizioneTeamMediator.iscriviTeam(iscrizioneDTO);
+        return iscrizioneTeamService.iscriviTeam(iscrizioneDTO);
     }
 }
